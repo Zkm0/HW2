1224d8ced01195bd209f5f28307448086bb04a28
Zoe <zoemk07@gmail.com>
2023-04-24 22:20:56 -0700

    draft2-1

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-// Define the original array
+
 let origArray = [2, 3, 4];
 
-// Add event listener for DOMContentLoaded
+
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Write the original array values into the textboxes
   document.getElementById('textbox1').value = origArray[0];
   document.getElementById('textbox2').value = origArray[1];
   document.getElementById('textbox3').value = origArray[2];
 
-  // Add event listener for the first button (square)
   document.getElementById('btn1').onclick = function() {
     let newArray = origArray.map(function(num) {
       return Math.pow(num, 2);
@@ -19,7 +17,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('textbox3').value = newArray[2];
   };
 
-  // Add event listener for the second button (cube)
   document.getElementById('btn2').addEventListener('click', function() {
     let newArray = origArray.map(function(num) {
       return Math.pow(num, 3);
@@ -29,7 +26,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('textbox3').value = newArray[2];
   });
 
-  // Add event listener for the third button (fourth power)
   function fourthPower() {
     let newArray = origArray.map(function(num) {
       return Math.pow(num, 4);
